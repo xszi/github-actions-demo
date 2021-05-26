86ee0c7cd694f63a3d89adb751bbcaeb9617447e
xszi <jiuhuaw@126.com>
2021-05-26 20:42:22 +0800

    vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+
+  resolve: {
+      alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'src')}
+      ]
+  },
+
+  server: {
+      port: 7000
+  },
+
+  build: {
+      outDir: 'build'
+  }
 })
