0845d19402c124b2285401707571cc27ef1fb922
xszi <jiuhuaw@126.com>
2021-05-27 17:37:31 +0800

    login page

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import './assets/styles/global.scss'
+import 'antd/dist/antd.css'
+import { ConfigProvider } from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import { Provider } from 'react-redux'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+import Routes from './router'
+import store from '@/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    //   <React.StrictMode>
+    <Provider store={store}>
+        <ConfigProvider locale={zh_CN}>
+            <Routes />
+        </ConfigProvider>
+    </Provider>
+    //   </React.StrictMode>,
+    ,
+    document.getElementById('root')
 )
---
+++ front/src/router/routes.ts
@@ -0,0 +1,4 @@
+import { lazy } from 'react'
+import { IRouteProps } from '@/router/types'
+import { HOME, SETTING } from './constants'
+import Login from '@/views/login/index'
